8c50c2621e189aed1634ea2f1419c0bc87b1c733
Fabián Delgado <fabian@bullquine.com>
2023-02-19 18:16:27 -0300

    fix name table users and create news tables
    Fix name table users
    
    New tables added:
    Treatment
    Medical Record

---
+++ migrations/20230219201916_migrate_treatments.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS treatments (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    frecuency VARCHAR(255) NOT NULL,
+    shots VARCHAR(255) NOT NULL,
+    date_start TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT FK_user FOREIGN KEY(user_id)
+    REFERENCES users(id)
+);
---
+++ migrations/20230219202933_migrate_medical_records.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS medical_records (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL,
+    health_care_provider_type BOOLEAN NOT NULL,
+    health_care_provider_public_name VARCHAR(100) NOT NULL,
+    emergency_medical_service BOOLEAN NOT NULL,
+    multiple_sclerosis_type VARCHAR(4) NOT NULL,
+    laboral_condition VARCHAR(100) NOT NULL
+    conmorbidity BOOLEAN NOT NULL,
+    treating_neurologist VARCHAR(100) NOT NULL,
+    support_network BOOLEAN NOT NULL,
+    is_disabled BOOLEAN NOT NULL,
+    educational_level VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT FK_user FOREIGN KEY(user_id)
+    REFERENCES users(id)
+);
