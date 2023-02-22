bc24e77a9b2c70e3994941e29aa40b21a79c2f9f
Fabián Delgado <fabian@bullquine.com>
2023-02-22 11:58:54 -0300

    added symptoms and migration tables

---
+++ migrations/20230219212134_migrate_symptoms.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS symptoms (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    is_active BOOLEAN NOT NULL,
+    scale CHAR(5)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_user FOREIGN KEY(user_id)
+    REFERENCES users(id)
+);
---
+++ migrations/20230222130703_migrate_symptom_user.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS symptom_user (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL,
+    symptom_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT FK_user FOREIGN KEY(user_id)
+    REFERENCES users(id),
+    
+    CONSTRAINT FK_symptom FOREIGN KEY(symptom_id)
+    REFERENCES symptoms(id)
+);
