884e35a6e5080aedb3398c44ce4c378e38098931
Fabián Delgado <fabian@bullquine.com>
2023-02-22 14:01:18 -0300

    added health_services and health_services_ratings

---
+++ migrations/20230222165803_migrate_health_services_ratings.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS health_services_ratings (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    health_services_id INT NOT NULL,
+    rating CHAR(1),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT FK_health_service FOREIGN KEY(health_services_id)
+    REFERENCES health_services(id),
+
+);
