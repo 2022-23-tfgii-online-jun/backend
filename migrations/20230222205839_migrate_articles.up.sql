2df2fd3cf1fe9d6399289236aea275bcb2003ab6
Fabián Delgado <fabian@bullquine.com>
2023-02-22 18:08:12 -0300

    added table articles and relation with categories

---
+++ migrations/20230222205839_migrate_articles.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS articles (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) DEFAULT NOT NULL,
+    image TEXT DEFAULT NULL,
+    content TEXT DEFAULT NULL,
+    is_published BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
+    CONSTRAINT FK_user FOREIGN KEY(user_id)
+    REFERENCES users(id)
+);
---
+++ migrations/20230222210439_migrate_article_categories.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS article_categories (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    category_id INT NOT NULL,
+    article_id INT NOT NULL,
+    image TEXT DEFAULT NULL,
+    content TEXT DEFAULT NULL,
+    is_published BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
+    CONSTRAINT FK_categorie FOREIGN KEY(category_id)
+    REFERENCES categories(id),
+    
+    CONSTRAINT FK_article FOREIGN KEY(article_id)
+    REFERENCES articles(id)
+);
+
+
+
+
