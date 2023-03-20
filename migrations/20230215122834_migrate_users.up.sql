164a4c0ad745adee254e4fe0f4618fcc8fede76c
Fabián Delgado <fabian@bullquine.com>
2023-03-19 21:31:03 -0300

    Remove form migrations NOT NULL
    Remove Not Null from the migration because it is necessary to create the user because otherwise all the fields should be requested when registering.
    These fields will be filled out in a separate process once registered.

--- migrations/20230215122834_migrate_users.up.sql
+++ migrations/20230215122834_migrate_users.up.sql
@@ -1,18 +1,19 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     uuid UUID NOT NULL DEFAULT gen_random_uuid(),
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
     profile_image TEXT,
     age INTEGER,
     sex CHAR(1),
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    user_type VARCHAR(255) NOT NULL,
+    user_type VARCHAR(255),
     is_active BOOLEAN DEFAULT true,
     is_banned BOOLEAN DEFAULT false,
     city VARCHAR(255),
     country VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	deleted_at TIMESTAMP WITH TIME ZONE
 );
