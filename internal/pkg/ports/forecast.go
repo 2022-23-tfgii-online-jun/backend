9cfeea5494294eb92d069242cb8c2414fdd84836
Fabián Delgado <fabian.delgado@outlook.com>
2023-06-11 19:19:00 -0300

    improvements in comments article, category and healthservice

--- internal/pkg/ports/forecast.go
+++ internal/pkg/ports/forecast.go
@@ -2,11 +2,23 @@ package ports
 
 import "github.com/emur-uy/backend/internal/pkg/entity"
 
+// ForecastService is an interface defining a contract for business logic operators related to Forecasts.
+// It works with the entity layer to manipulate Forecast data.
 type ForecastService interface {
+	// GetDistinctCountryAndCityUsers retrieves distinct users based on their country and city.
+	// Returns a slice of User entities and an error if any occurred.
 	GetDistinctCountryAndCityUsers() ([]entity.User, error)
 }
 
+// ForecastRepository is an interface that acts as a contract for the data access layer,
+// requiring implementations to provide methods for querying and modifying forecast data.
 type ForecastRepository interface {
+	// Create takes a new Forecast value and adds it to the data store.
+	// Returns an error if the operation fails.
 	Create(value interface{}) error
+
+	// GetDistinctCountryAndCityUsers retrieves distinct users based on their country and city,
+	// modifying the provided slice of Users.
+	// Returns an error if the operation fails.
 	GetDistinctCountryAndCityUsers(users *[]entity.User) error
 }
--- internal/pkg/ports/healthservice.go
+++ internal/pkg/ports/healthservice.go
@@ -6,30 +6,46 @@ import (
 	"github.com/google/uuid"
 )
 
-// HealthServiceRepository is the interface that defines the methods for accessing the health service data store.
+// HealthServiceRepository is an interface that acts as a contract for the data access layer,
+// requiring implementations to provide methods for querying and modifying Health Service data.
 type HealthServiceRepository interface {
+	// FindByUUID retrieves a HealthService based on its UUID.
+	// Returns the HealthService and an error if any occurred.
 	FindByUUID(uuid uuid.UUID, out interface{}) (interface{}, error)
 
-	// CreateWithOmit creates a new health service record while omitting specific fields.
+	// Create takes a new HealthService value and adds it to the data store.
 	// Returns an error if the operation fails.
 	Create(value interface{}) error
 
-	// UpdateHealthService updates an existing health service record with the provided health service data.
+	// Update updates an existing HealthService value in the data store.
 	// Returns an error if the operation fails.
 	Update(value interface{}) error
 
-	// First retrieves the first record that matches the given conditions from the database
+	// First retrieves the first record that matches the given conditions from the database.
 	// Returns an error if the operation fails.
 	First(out interface{}, conditions ...interface{}) error
 
+	// Find retrieves all HealthService values that match the given conditions from the database.
+	// Returns an error if the operation fails.
 	Find(out interface{}, conditions ...interface{}) error
 
+	// Delete removes an existing HealthService from the data store.
+	// Returns an error if the operation fails.
 	Delete(out interface{}) error
 }
 
-// HealthServiceService is the interface that defines the methods for managing health services in the application.
+// HealthServiceService is an interface defining a contract for business logic operators related to Health Services.
+// It works with the entity layer to manipulate Health Service data.
 type HealthServiceService interface {
+	// CreateHealthService takes a request to create a Health Service.
+	// Returns the Health Service ID, status, and an error if any occurred.
 	CreateHealthService(c *gin.Context, createReq *entity.RequestCreateHealthService) (string, int, error)
+
+	// GetAllHealthServices retrieves all Health Services from the data store.
+	// Returns a slice of HealthService entities and an error if any occurred.
 	GetAllHealthServices() ([]*entity.HealthService, error)
+
+	// AddRatingToHealthService adds a rating to a specific Health Service.
+	// Returns the status and an error if any occurred.
 	AddRatingToHealthService(rating *entity.HealthServiceRating) (int, error)
 }
