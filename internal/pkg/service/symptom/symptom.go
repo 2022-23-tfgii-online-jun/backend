09aeddc818670bda4cc0723b4b20bfd3446b5c0b
Fabián Delgado <fabian.delgado@outlook.com>
2023-06-11 21:37:59 -0300

    add comments and manage errors in symptom

--- internal/pkg/service/symptom/symptom.go
+++ internal/pkg/service/symptom/symptom.go
@@ -43,7 +43,7 @@ func (s *service) CreateSymptom(c *gin.Context, createReq *entity.RequestCreateS
 	// Save the symptom to the database
 	err := s.repo.CreateWithOmit("uuid", symptom)
 	if err != nil {
-		return nil, http.StatusInternalServerError, fmt.Errorf("error creating symptom: %s", err)
+		return nil, http.StatusInternalServerError, fmt.Errorf("%w: %s", ErrCreatingSymptom, err)
 	}
 
 	// Return the symptom and the HTTP OK status code if the create operation is successful
@@ -55,7 +55,7 @@ func (s *service) GetAllSymptoms() ([]*entity.Symptom, error) {
 	// Get all symptoms from the database
 	var symptoms []*entity.Symptom
 	if err := s.repo.Find(&symptoms); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%w: %s", ErrFindingSymptom, err)
 	}
 
 	return symptoms, nil
@@ -64,86 +64,39 @@ func (s *service) GetAllSymptoms() ([]*entity.Symptom, error) {
 // AddUserToSymptom adds a user to a symptom.
 func (s *service) AddUserToSymptom(userUUID uuid.UUID, symptomUser *entity.RequestCreateSymptomUser) (int, error) {
 	// Find user by UUID
-	user := &entity.User{}
-	foundUser, err := s.repo.FindByUUID(userUUID, user)
+	user, err := s.repo.FindByUUID(userUUID, &entity.User{})
 	if err != nil {
-		return http.StatusInternalServerError, fmt.Errorf("error finding user: %s", err)
+		return http.StatusInternalServerError, fmt.Errorf("%w: %s", ErrFindingSymptom, err)
 	}
 
-	// Perform type assertion to convert foundUser to *entity.User
-	userEntity, ok := foundUser.(*entity.User)
+	// Ensure the found entity is of type *entity.User
+	userEntity, ok := user.(*entity.User)
 	if !ok {
 		return http.StatusInternalServerError, ErrTypeAssertionFailed
 	}
 
 	// Find symptom by UUID
-	symptom := &entity.Symptom{}
-	foundSymptom, err := s.repo.FindByUUID(symptomUser.SymptomUUID, symptom)
+	symptom, err := s.repo.FindByUUID(symptomUser.SymptomUUID, &entity.Symptom{})
 	if err != nil {
-		return http.StatusInternalServerError, fmt.Errorf("error finding symptom: %s", err)
+		return http.StatusInternalServerError, fmt.Errorf("%w: %s", ErrFindingSymptom, err)
 	}
 
-	// Perform type assertion to convert foundSymptom to *entity.Symptom
-	symptomEntity, ok := foundSymptom.(*entity.Symptom)
+	// Ensure the found entity is of type *entity.Symptom
+	symptomEntity, ok := symptom.(*entity.Symptom)
 	if !ok {
 		return http.StatusInternalServerError, ErrTypeAssertionFailed
 	}
 
 	// Create a new record for the user and symptom
-	symptomUser := &entity.SymptomUser{
+	record := &entity.SymptomUser{
 		UserID:    userEntity.ID,
 		SymptomID: symptomEntity.ID,
 	}
 
 	// Add the user to the symptom
-	err = s.repo.Create(symptomUser)
+	err = s.repo.Create(record)
 	if err != nil {
-		return http.StatusInternalServerError, ErrAddingUserToSymptom
-	}
-
-	// Return the HTTP OK status code if the operation is successful
-	return http.StatusOK, nil
-}
-
-// RemoveUserFromSymptom removes a user from a symptom.
-func (s *service) RemoveUserFromSymptom(userUUID uuid.UUID, req *entity.RequestCreateSymptomUser) (int, error) {
-	// Find user by UUID
-	user := &entity.User{}
-	foundUser, err := s.repo.FindByUUID(userUUID, user)
-	if err != nil {
-		return http.StatusInternalServerError, fmt.Errorf("error finding user: %s", err)
-	}
-
-	// Perform type assertion to convert foundUser to *entity.User
-	userEntity, ok := foundUser.(*entity.User)
-	if !ok {
-		return http.StatusInternalServerError, ErrTypeAssertionFailed
-	}
-
-	// Find symptom by UUID
-	symptom := &entity.Symptom{}
-	foundSymptom, err := s.repo.FindByUUID(req.SymptomUUID, symptom)
-	if err != nil {
-		return http.StatusInternalServerError, fmt.Errorf("error finding symptom: %s", err)
-	}
-
-	// Perform type assertion to convert foundSymptom to *entity.Symptom
-	symptomEntity, ok := foundSymptom.(*entity.Symptom)
-	if !ok {
-		return http.StatusInternalServerError, ErrTypeAssertionFailed
-	}
-
-	// Find symptom user by user and symptom IDs
-	symptomUser := &entity.SymptomUser{}
-	err = s.repo.FindItemByIDs(userEntity.ID, symptomEntity.ID, "symptom_users", "user_id", "symptom_id", symptomUser)
-	if err != nil {
-		return http.StatusInternalServerError, ErrFindingSymptomUser
-	}
-
-	// Remove the user from the symptom
-	err = s.repo.Delete(symptomUser)
-	if err != nil {
-		return http.StatusInternalServerError, ErrRemovingUserFromSymptom
+		return http.StatusInternalServerError, fmt.Errorf("%w: %s", ErrAddingUserToSymptom, err)
 	}
 
 	// Return the HTTP OK status code if the operation is successful
@@ -153,35 +106,77 @@ func (s *service) RemoveUserFromSymptom(userUUID uuid.UUID, req *entity.RequestC
 // GetSymptomsByUser returns all symptoms related to a user.
 func (s *service) GetSymptomsByUser(userUUID uuid.UUID) ([]*entity.Symptom, error) {
 	// Find user by UUID
-	user := &entity.User{}
-	foundUser, err := s.repo.FindByUUID(userUUID, user)
+	user, err := s.repo.FindByUUID(userUUID, &entity.User{})
 	if err != nil {
 		return nil, fmt.Errorf("error finding user: %s", err)
 	}
 
-	// Perform type assertion to convert foundUser to *entity.User
-	userEntity, ok := foundUser.(*entity.User)
+	// Ensure the found entity is of type *entity.User
+	userEntity, ok := user.(*entity.User)
 	if !ok {
-		return nil, ErrTypeAssertionFailed
+		return nil, fmt.Errorf("error asserting user entity type")
 	}
 
-	// Find symptom IDs related to the user from the repository
+	// Get the symptom user IDs from the repository
 	var symptomUserIDs []*entity.SymptomUser
 	err = s.repo.Find(&symptomUserIDs, "user_id = ?", userEntity.ID)
 	if err != nil {
 		return nil, fmt.Errorf("error finding symptom user IDs: %s", err)
 	}
 
-	// Get symptoms based on the obtained IDs
+	// Get the symptoms based on the obtained IDs
 	symptoms := make([]*entity.Symptom, 0, len(symptomUserIDs))
 	for _, symptomUser := range symptomUserIDs {
-		symptom := &entity.Symptom{}
-		err := s.repo.FindByID(symptomUser.SymptomID, symptom)
+		var symptom entity.Symptom
+		err := s.repo.Find(&symptom, "id = ?", symptomUser.SymptomID)
 		if err != nil {
 			return nil, fmt.Errorf("error finding symptom by ID: %s", err)
 		}
-		symptoms = append(symptoms, symptom)
+		symptoms = append(symptoms, &symptom)
 	}
 
 	return symptoms, nil
+}
+
+// RemoveUserFromSymptom removes a user from a symptom.
+func (s *service) RemoveUserFromSymptom(userUUID uuid.UUID, req *entity.RequestCreateSymptomUser) (int, error) {
+	// Find user by UUID
+	user, err := s.repo.FindByUUID(userUUID, &entity.User{})
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("error finding user: %s", err)
+	}
+
+	// Ensure the found entity is of type *entity.User
+	userEntity, ok := user.(*entity.User)
+	if !ok {
+		return http.StatusInternalServerError, fmt.Errorf("error asserting user entity type")
+	}
+
+	// Find symptom by UUID
+	symptom, err := s.repo.FindByUUID(req.SymptomUUID, &entity.Symptom{})
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("error finding symptom: %s", err)
+	}
+
+	// Ensure the found entity is of type *entity.Symptom
+	symptomEntity, ok := symptom.(*entity.Symptom)
+	if !ok {
+		return http.StatusInternalServerError, fmt.Errorf("error asserting symptom entity type")
+	}
+
+	// Find symptom user by user and symptom IDs
+	var symptomUser entity.SymptomUser
+	err = s.repo.Find(&symptomUser, "user_id = ? AND symptom_id = ?", userEntity.ID, symptomEntity.ID)
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("error finding SymptomUser: %s", err)
+	}
+
+	// Remove the user from the symptom
+	err = s.repo.Delete(&symptomUser)
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("error removing user from symptom: %s", err)
+	}
+
+	// Return the HTTP OK status code if the operation is successful
+	return http.StatusOK, nil
 }
