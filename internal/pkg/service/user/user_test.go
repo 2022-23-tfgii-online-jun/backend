7f0c079e8cd806b0233a85a407e8797d1dbd86ac
Fabián Delgado <fabian.delgado@outlook.com>
2023-07-04 16:35:40 -0300

    remove .env

--- internal/pkg/service/user/user_test.go
+++ internal/pkg/service/user/user_test.go
@@ -2,7 +2,9 @@ package user_test
 
 import (
 	"errors"
+	"log"
 	"net/http"
+	"os"
 	"testing"
 	"time"
 
@@ -18,7 +20,7 @@ import (
 var testUuid = uuid.MustParse("24df3f36-ca63-11ed-afa1-0242ac120002")
 
 // Define a test email for testing purposes.
-const testEmail = "test@example.com"
+const testEmail = "test@gmail.com"
 
 // Define a test password for testing purposes.
 const testPassword = "$2a$08$dawrvtxhIXVQZ0pz7o809uSsNkSaZp2gW2vwNQERHn37bwFOWIku." // bcrypt hash for "password"
@@ -39,7 +41,16 @@ func (m *MockUserRepository) Update(value interface{}) error {
 // First is a mock implementation of the First method.
 func (m *MockUserRepository) First(out interface{}, conditions ...interface{}) error {
 	if conditions[0] == "uuid= ?" && conditions[1] == testUuid {
+		out.(*entity.User).ID = 1
 		out.(*entity.User).UUID = testUuid
+		out.(*entity.User).Email = testEmail
+		out.(*entity.User).Password = testPassword
+		out.(*entity.User).IsActive = true
+		out.(*entity.User).CreatedAt = time.Now()
+		out.(*entity.User).UpdatedAt = time.Now()
+		out.(*entity.User).FirstName = "zIjgMmYc9f4F4uHGm2Pz4BcLgoo="
+		out.(*entity.User).LastName = "p4hOAyWxm9E0-FIcc_J74jtLK3w="
+		out.(*entity.User).ProfileImage = "8N5pWzM8JvcgklYlvjng5MjmSOA="
 		return nil
 	}
 	if conditions[0] == "email = ?" && conditions[1] == testEmail {
@@ -50,6 +61,9 @@ func (m *MockUserRepository) First(out interface{}, conditions ...interface{}) e
 		out.(*entity.User).IsActive = true
 		out.(*entity.User).CreatedAt = time.Now()
 		out.(*entity.User).UpdatedAt = time.Now()
+		out.(*entity.User).FirstName = "zIjgMmYc9f4F4uHGm2Pz4BcLgoo="
+		out.(*entity.User).LastName = "p4hOAyWxm9E0-FIcc_J74jtLK3w="
+		out.(*entity.User).ProfileImage = "8N5pWzM8JvcgklYlvjng5MjmSOA="
 		return nil
 	}
 	if conditions[0] == "user_id = ?" && conditions[1] == 1 {
@@ -92,6 +106,60 @@ func (m *MockUserRepository) FindByUUID(userUUID uuid.UUID, out interface{}) (in
 	return nil, errors.New("user not found")
 }
 
+const tmpEnvFileName = "prod.env"
+
+func createTempEnv() {
+	// Prepare environment variables for testing.
+	// Create a temporary file for the environment variables.
+	tmpFile, err := os.Create(tmpEnvFileName)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+	// Write test data to the temporary file.
+	testData := []byte("DB_HOST=localhost\n" +
+		"DB_USER=test\n" +
+		"DB_PASS=test\n" +
+		"DB_NAME=test\n" +
+		"DB_PORT=25060\n" +
+		"DB_TLS=require\n" +
+		"SENTRY_KEY=test\n" +
+		"GIN_MODE=debug\n" +
+		"APP_ENV=dev\n" +
+		"SECRET_KEY=test\n" +
+		"JWT_TOKEN_KEY=07bdb5e4afedc99c756075c6403122b622e070bb314eb4e8e2127c22794a392acda82ab9bb61b246015404bd58d38aab3b4488eb087d944a837b2da0d15ceb5b\n" +
+		"JWT_TOKEN_EXPIRED=24\n" +
+		"AWS_BUCKET_NAME=test\n" +
+		"AWS_FOLDER_NAME=test\n" +
+		"AWS_REGION_NAME=test\n" +
+		"AWS_ACCESS_KEY=test\n" +
+		"AWS_SECRET_KEY=test\n" +
+		"AWS_ENDPOINT=test\n" +
+		"FORECAST_KEY=test\n" +
+		"FORECAST_API=test\n" +
+		"ENCRYPTION_KEY=!mGL^CiiDhVVLaR7dM%FCeymgGuq^RUQ\n")
+	_, err = tmpFile.Write(testData)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+	// Close the temporary file to flush its contents to disk.
+	err = tmpFile.Close()
+	if err != nil {
+		log.Printf(err.Error())
+	}
+}
+
+func TestMain(m *testing.M) {
+
+	//create temp env file
+	createTempEnv()
+
+	// Run the tests
+	_ = m.Run()
+
+	// Defer cleanup of the temporary file.
+	defer os.Remove(tmpEnvFileName)
+}
+
 // TestLogin checks the Login method of the UserService.
 // This test function verifies that the UserService's Login method works as expected.
 func TestLogin(t *testing.T) {
@@ -105,7 +173,7 @@ func TestLogin(t *testing.T) {
 		credentials *entity.DefaultCredentials
 		expectError bool
 	}{
-		{"successful login", &entity.DefaultCredentials{Email: "test@example.com", Password: "password"}, false},
+		{"successful login", &entity.DefaultCredentials{Email: "test@gmail.com", Password: "password"}, false},
 		{"failed login - incorrect email", &entity.DefaultCredentials{Email: "nonexistent@example.com", Password: "password"}, true},
 		{"failed login - incorrect password", &entity.DefaultCredentials{Email: "test@example.com", Password: "wrong_password"}, true},
 	}
@@ -138,8 +206,11 @@ func TestCreateUser(t *testing.T) {
 
 	// Case 1: Valid user, should return HTTP status 201.
 	u := &entity.User{
-		Email:    "test@example.com",
-		Password: "password",
+		Email:        "test@gmail.com",
+		Password:     "password",
+		FirstName:    "abcd",
+		LastName:     "efgh",
+		ProfileImage: "1234",
 	}
 	status, err := s.CreateUser(u)
 	assert.Nil(t, err)
@@ -161,34 +232,39 @@ func TestUpdateUser(t *testing.T) {
 	mockRepo := &MockUserRepository{}
 	s := user.NewService(mockRepo)
 
-	dateOfBirth := time.Now()
+	dateOfBirth := time.Now().String()
 
-	// Case 1: Valid update data, should return HTTP status 200.
+	// Case 1: User exists & Valid update data, should return HTTP status 200.
 	updateData := &entity.UpdateUser{
 		FirstName:   stringPtr("John"),
 		LastName:    stringPtr("Doe"),
-		DateOfBirth: &dateOfBirth,
+		DateOfBirth: dateOfBirth,
 		Sex:         stringPtr("M"),
 		UserType:    stringPtr("admin"),
 		City:        stringPtr("Montevideo"),
 		Country:     stringPtr("UY"),
 	}
 
-	status, err := s.UpdateUser(updateData)
+	status, err := s.UpdateUser(testUuid, updateData)
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusOK, status)
 
-	// Case 2: Invalid update data, should return HTTP status 500.
+	// Case 2: user doesn't exist & Valid update data, should return HTTP status 404.
+	status, err = s.UpdateUser(uuid.New(), updateData)
+	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusInternalServerError, status)
+
+	// Case 3: User exists & Invalid update data, should return HTTP status 500.
 	updateData = &entity.UpdateUser{
 		FirstName:   stringPtr("John"),
 		LastName:    stringPtr("Doe"),
-		DateOfBirth: &dateOfBirth,
+		DateOfBirth: dateOfBirth,
 		Sex:         stringPtr("M"),
 		UserType:    stringPtr("admin"),
 		City:        nil,
 		Country:     nil,
 	}
-	status, err = s.UpdateUser(updateData)
+	status, err = s.UpdateUser(testUuid, updateData)
 	assert.NotNil(t, err)
 	assert.Equal(t, http.StatusInternalServerError, status)
 }
@@ -197,6 +273,7 @@ func TestGetUser(t *testing.T) {
 	// Initialize the mock repository and service.
 	mockRepo := &MockUserRepository{}
 	s := user.NewService(mockRepo)
+
 	// Test case 1: user found
 	mockUser, err := s.GetUser(testUuid)
 	assert.Nil(t, err)
